847938dc3a0c71302177221d653df5e56f60e741
Oleg Khryashchev <oleg.khryashchev@rating.nocal.quadinfo.com>
2018-08-30 16:29:03 -0700

    added Jobs functionality

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -79,8 +79,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'portfoliodb',
-        'USER': 'postgres',
-        'PASSWORD': '~wmssi123',
+        'USER': 'djangodbman',
+        'PASSWORD': 'django1234',
         'HOST': 'localhost',
         'PORT': '5432'
 
