6f72f923b2be1c6c5e8abebc5334e01fd3412c74
Oleg Khryashchev <oleg.khryashchev@rating.nocal.quadinfo.com>
2018-08-30 16:26:45 -0700

    added Jobs functionality

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Blog(models.Model):
+    dt = timezone.now()
+
+    title = models.CharField(max_length=255, default='')
+    pub_date = models.DateTimeField(default=dt)
+    body = models.TextField(default='')
+    image = models.ImageField(upload_to='images/', null=True)
